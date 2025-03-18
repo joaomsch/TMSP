0be518e47d43fa0b088667be8e19f16b9eada051
joaomsch <pf28ywm@eduvaud.ch>
2025-03-18 16:24:07 +0100

    V2 avec une classe

--- Program.cs
+++ Program.cs
@@ -1,95 +1,69 @@
-///Auteur: Joao Mascarenhas
-///Lieu: ETML
-///Date: 3 mars 2025
-///Description: C'est le code pour lénigme du cavalier en C#
-using System.ComponentModel.Design;
-using System.Timers;
-//Commentaires : idée : un deuxième niveau avec sans le déplacement en bleu et un troisième sans avec du temps avec un système de points.
+/// Auteur: Joao Mascarenhas
+/// Lieu: ETML
+/// Date: 3 mars 2025
+/// Description: C'est le code pour l'énigme du cavalier en C#
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
 class Program
 {
-    static char[,] plateau;
     static int taille = 8;
-
-    static int[] deplacementsCavalierX = new int[] { 2, 2, -2, -2, 1, 1, -1, -1 };
-    static int[] deplacementsCavalierY = new int[] { 1, -1, 1, -1, 2, -2, 2, -2 };
-    static List<string> visitedPositions = new List<string>();
-
+    Cavalier cavalier = new Cavalier(8);
     static void Main()
     {
-        // Affichage du titre
-        string cavaliertitre = @"
-    ██╗     ███████╗     ██████╗ █████╗ ██╗   ██╗ █████╗ ██╗     ██╗███████╗██████╗ 
-    ██║     ██╔════╝    ██╔════╝██╔══██╗██║   ██║██╔══██╗██║     ██║██╔════╝██╔══██╗
-    ██║     █████╗      ██║     ███████║██║   ██║███████║██║     ██║█████╗  ██████╔╝
-    ██║     ██╔══╝      ██║     ██╔══██║╚██╗ ██╔╝██╔══██║██║     ██║██╔══╝  ██╔══██╗
-    ███████╗███████╗    ╚██████╗██║  ██║ ╚████╔╝ ██║  ██║███████╗██║███████╗██║  ██║
-    ╚══════╝╚══════╝     ╚═════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚═╝╚══════╝╚═╝  ╚═╝
-                                                                                
-";
-        Console.WriteLine(cavaliertitre);
+        Cavalier cavalier = new Cavalier(taille);
+        cavalier.Titre();
         Thread.Sleep(1500);
 
         Console.WriteLine("Voulez-vous jouer au jeu du cavalier? (Oui/Non)");
-        string reponseJouer = Console.ReadLine();
+        string reponseJouer = Console.ReadLine().ToLower();
 
-        if (reponseJouer.ToLower() == "non")
+        if (reponseJouer == "non")
         {
             Console.Clear();
             Console.WriteLine("Jeu du Cavalier terminé");
             return;
         }
 
-        if (reponseJouer.ToLower() == "oui")
+        if (reponseJouer == "oui")
         {
-            Console.WriteLine("Niveau 1");
             Console.Clear();
-            plateau = new char[taille, taille];
-
-            for (int i = 0; i < taille; i++)
-                for (int j = 0; j < taille; j++)
-                    plateau[i, j] = ' ';
+            cavalier.InitialiserPlateau();
 
             Console.WriteLine("Entrez la position de départ du cavalier (ex: A1, B3, etc.) :");
-            string position = ObtenirPositionValidedif();
-            visitedPositions.Add(position);
+            string position = cavalier.ObtenirPositionValidedif();
+            
 
             int x = taille - (position[1] - '0');
             int y = position[0] - 'A';
+            cavalier.RecordLastPosition(x, y);
 
-            plateau[x, y] = 'C';
-            AfficherPlateaudif(x, y);
+            cavalier.MarquerPositionVisitee(x, y);
+            cavalier.AfficherPlateaudif(x, y);
 
             while (true)
             {
                 Console.WriteLine("\nDéplacez le cavalier !");
-                string deplacement = ObtenirPositionValidedif();
+                string deplacement = cavalier.ObtenirPositionValidedif();
 
                 int deplacementX = taille - (deplacement[1] - '0');
                 int deplacementY = deplacement[0] - 'A';
 
-                bool mouvementValide = false;
-                for (int i = 0; i < 8; i++)
-                {
-                    int nouvelleX = x + deplacementsCavalierX[i];
-                    int nouvelleY = y + deplacementsCavalierY[i];
-
-                    if (nouvelleX == deplacementX && nouvelleY == deplacementY)
-                    {
-                        mouvementValide = true;
-                        break;
-                    }
-                }
+                bool mouvementValide = cavalier.VerifierMouvement(x, y, deplacementX, deplacementY);
 
                 if (mouvementValide)
                 {
-                    plateau[x, y] = ' ';
                     x = deplacementX;
                     y = deplacementY;
 
-                    if (!visitedPositions.Contains(deplacement))
+                    
+
+                    if (!cavalier.VisitedPositions.Contains(deplacement))
                     {
-                        visitedPositions.Add(deplacement);
-                        if (visitedPositions.Count == 64)
+                        cavalier.MarquerPositionVisitee(x, y);
+                        if (cavalier.VisitedPositions.Count == taille * taille)
                         {
                             Console.Clear();
                             Console.WriteLine("Félicitations ! Vous avez réussi l'énigme.");
@@ -102,8 +76,8 @@ class Program
                         break;
                     }
 
-                    plateau[x, y] = 'C';
-                    AfficherPlateaudif(x, y);
+                    cavalier.AfficherPlateaudif(x, y);
+                    cavalier.RecordLastPosition(x, y);
                 }
                 else
                 {
@@ -111,85 +85,5 @@ class Program
                 }
             }
         }
-    }
-
-    static string ObtenirPositionValidedif()
-    {
-        while (true)
-        {
-            string position = Console.ReadLine().ToUpper();
-
-            if (position.Length == 2 &&
-                position[0] >= 'A' && position[0] <= 'H' &&
-                position[1] >= '1' && position[1] <= '8')
-            {
-                return position;
-            }
-            else
-            {
-                Console.WriteLine("Position invalide. Entrez une position valide (ex: A1, B3, etc.).");
-            }
-        }
-    }
-
-    static void AfficherPlateaudif(int cavalierX, int cavalierY)
-    {
-        Console.Clear();
-
-        Console.Write("    ");
-        for (int i = 0; i < taille; i++)
-            Console.Write("  " + (char)(65 + i) + " ");
-        Console.WriteLine();
-
-        for (int i = 0; i < taille; i++)
-        {
-            Console.Write("    ");
-            for (int j = 0; j < taille; j++)
-                Console.Write("╬═══");
-            Console.WriteLine("╣");
-
-            Console.Write("  " + (8 - i) + " ");
-            for (int j = 0; j < taille; j++)
-            {
-                if (visitedPositions.Contains($"{(char)(j + 'A')}{8 - i}"))
-                {
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.Write("║ " + plateau[i, j] + " ");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    bool mouvementValide = false;
-                    for (int k = 0; k < 8; k++)
-                    {
-                        int nouvelleposX = cavalierX + deplacementsCavalierX[k];
-                        int nouvelleposY = cavalierY + deplacementsCavalierY[k];
-
-                        if (i == nouvelleposX && j == nouvelleposY && !visitedPositions.Contains($"{(char)(j + 'A')}{8 - i}"))
-                        {
-                            mouvementValide = true;
-                            break;
-                        }
-                    }
-
-                    if (mouvementValide)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                        Console.Write("║ " + plateau[i, j] + " ");
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        Console.Write("║ " + plateau[i, j] + " ");
-                    }
-                }
-            }
-            Console.WriteLine("║");
-        }
-
-        Console.Write("    ");
-        for (int j = 0; j < taille; j++)
-            Console.Write("╬═══");
-        Console.WriteLine("╣");
     }
 }
