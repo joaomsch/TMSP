4467031c6f64f63799c2a8088e21ce021fd0df23
joaomsch <pf28ywm@eduvaud.ch>
2025-04-11 09:04:38 +0200

    Version Finale

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 /// Auteur: Joao Mascarenhas
 /// Lieu: ETML
 /// Date: 11 avril 2025
-/// Description: C'est le code pour l'énigme du cavalier en C#
+/// Description: Le code pour l'énigme du cavalier en C#
 
 using System;
 using System.Collections.Generic;
@@ -13,29 +13,41 @@ class Program
     Cavalier cavalier = new Cavalier(8);
     static void Main()
     {
+        // Création d'une instance de la classe Cavalier
         Cavalier cavalier = new Cavalier(taille);
+
+        // Affiche le titre du jeu
         cavalier.Titre();
+        // Pause de 1.5 secondes
         Thread.Sleep(1500);
 
         Console.WriteLine("Voulez-vous jouer au jeu du cavalier? (Oui/Non)");
         string reponseJouer = Console.ReadLine().ToLower();
 
+        // Si l'utilisateur ne veut pas jouer, quitter le jeu
         if (reponseJouer == "non")
         {
             Console.Clear();
             Console.WriteLine("Jeu du Cavalier terminé");
             return;
         }
-
+        // Si l'utilisateur veut jouer
         if (reponseJouer == "oui")
         {
             Console.Clear();
-            Console.WriteLine("1. Déplacements possibles en bleu, case visitées en rouge, temps illimité");
-            Console.WriteLine("2. Cases visitées en rouge, temps illimité");
-            Console.WriteLine("3. Cases visitées en rouge, temps limité (6 minutes)");
+            // Affichage du menu de sélection du niveau
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine("║Dans cette énigme vous devez déplacer sur toutes les cases de l'échiquier en passant une seule fois par case.║");
+            Console.WriteLine("║1. Déplacements possibles en bleu, case visitées en rouge, temps illimité                                    ║");
+            Console.WriteLine("║2. Cases visitées en rouge, temps illimité                                                                   ║");
+            Console.WriteLine("║3. Cases visitées en rouge, temps limité (6 minutes)                                                         ║");
+            Console.WriteLine("╚═════════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+
+            Console.WriteLine("\nChaque niveau comporte un systéme de points, plus le niveau est compliqué, plus de points vous récoltez.");
             Console.WriteLine("\nA quel niveau voulez-vous jouer?(1, 2, 3):");
             string choixniveau = Console.ReadLine();
-
+            Console.WriteLine("Quel est votre nom de joueur?");
+            string pseudo = Console.ReadLine();
 
             if (choixniveau == "1")
             {
@@ -44,37 +56,44 @@ class Program
                 Console.Clear();
                 cavalier.InitialiserPlateau();
                 cavalier.AfficherPlateau3(0, 0, true);
+                cavalier.Points1();
                 Console.WriteLine("Entrez la position de départ du cavalier (ex: A1, B3, etc.) :");
                 string position = cavalier.ObtenirPositionValidedif();
 
-
+                // Conversion de la position (ex: A1 => x=7, y=0)
                 int x = taille - (position[1] - '0');
                 int y = position[0] - 'A';
-                cavalier.RecordLastPosition(x, y);
 
+                // Enregistre la position
+                cavalier.RecordLastPosition(x, y);
                 cavalier.MarquerPositionVisitee(x, y);
+
+                // Affiche le plateau avec la nouvelle position
                 cavalier.AfficherPlateau1(x, y);
 
                 while (true)
                 {
+                    Console.WriteLine("Joueur: " + pseudo);
+                    // Affiche les points
+                    cavalier.Points1();
                     Console.WriteLine("\nDéplacez le cavalier !");
                     string deplacement = cavalier.ObtenirPositionValidedif();
-
                     int deplacementX = taille - (deplacement[1] - '0');
                     int deplacementY = deplacement[0] - 'A';
 
+                    // Vérifie si le déplacement est valide pour un cavalier
                     bool mouvementValide = cavalier.VerifierMouvement(x, y, deplacementX, deplacementY);
-
                     if (mouvementValide)
                     {
+                        
                         x = deplacementX;
                         y = deplacementY;
 
-
-
+                        // Si la case n’a pas encore été visitée
                         if (!cavalier.VisitedPositions.Contains(deplacement))
                         {
                             cavalier.MarquerPositionVisitee(x, y);
+                            // Vérifie si toutes les cases ont été visitées
                             if (cavalier.VisitedPositions.Count == taille * taille)
                             {
                                 Console.Clear();
@@ -83,6 +102,7 @@ class Program
 
                             }
                         }
+                        
                         else
                         {
                             Console.WriteLine("PERDU ! Vous avez déjà visité cette case. Vous avez malheureusement pas résolu l'éngime du cavalier niveau 1.");
@@ -105,11 +125,11 @@ class Program
                 Thread.Sleep(1000);
                 Console.Clear();
                 cavalier.InitialiserPlateau();
+                cavalier.Points2();
                 cavalier.AfficherPlateau2(0, 0, true);
 
                 Console.WriteLine("Entrez la position de départ du cavalier (ex: A1, B3, etc.) :");
                 string position = cavalier.ObtenirPositionValidedif();
-
 
                 int x = taille - (position[1] - '0');
                 int y = position[0] - 'A';
@@ -120,6 +140,8 @@ class Program
 
                 while (true)
                 {
+                    Console.WriteLine("Joueur: " + pseudo);
+                    cavalier.Points2();
                     Console.WriteLine("\nDéplacez le cavalier !");
                     string deplacement = cavalier.ObtenirPositionValidedif();
 
@@ -167,8 +189,9 @@ class Program
                 Thread.Sleep(1000);
                 Console.Clear();
                 cavalier.InitialiserPlateau();
-
                 cavalier.AfficherPlateau3(0, 0, true);
+                cavalier.Points3();
+
 
                 Console.WriteLine("Entrez la position de départ du cavalier (ex: A1, B3, etc.) :");
                 string position = cavalier.ObtenirPositionValidedif();
@@ -180,6 +203,7 @@ class Program
                 cavalier.MarquerPositionVisitee(x, y);
                 cavalier.AfficherPlateau3(x, y);
 
+                // Timer 6 minutes en arrière-plan
                 new Thread(() =>
                 {
                     for (int i = 0; i < 360; i++)
@@ -194,6 +218,7 @@ class Program
 
                         Thread.Sleep(1000);
                     }
+                    // Fin du temps
                     Console.Clear();
                     Console.WriteLine("Vous avez plus assez de temps. Vous avez malheureusement pas résolu l'éngime du cavalier niveau 3.");
                     Environment.Exit(0);
@@ -202,6 +227,9 @@ class Program
 
                 while (true)
                 {
+                    Console.WriteLine("Joueur: " + pseudo);
+                    Console.WriteLine();
+                    cavalier.Points3();
                     Console.WriteLine("\nDéplacez le cavalier !");
                     string deplacement = cavalier.ObtenirPositionValidedif();
 
@@ -216,7 +244,6 @@ class Program
                         y = deplacementY;
 
 
-
                         if (!cavalier.VisitedPositions.Contains(deplacement))
                         {
                             cavalier.MarquerPositionVisitee(x, y);
@@ -228,6 +255,7 @@ class Program
 
                             }
                         }
+
                         else
                         {
                             Console.WriteLine("PERDU ! Vous avez déjà visité cette case. Vous avez malheureusement pas résolu l'éngime du cavalier niveau 3.");
